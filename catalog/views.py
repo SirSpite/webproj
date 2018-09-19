73767cd18e78db908afe87fd1af2301902330035
SirSpite <dduality48@gmail.com>
2018-09-19 11:36:18 +0300

    upd templates to display products

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
 def product_list(request):
-    return render(request, 'catalog/product_list.html', {})
+    products = Product.objects.all()
+    return render(request, 'catalog/product_list.html', {'products' : products})
